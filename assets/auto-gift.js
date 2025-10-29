cd5aa08b0faa2e175108d8575bc6ee099aa117bc
Duc Anh <ducanh260204@gmail.com>
2025-10-29 04:40:57 +0000

    .

--- assets/auto-gift.js
+++ assets/auto-gift.js
@@ -42,13 +42,14 @@ async function checkGift() {
 checkGift();
 
 // Theo dõi khi người dùng thay đổi giỏ
-document.addEventListener('click', function (e) {
+document.addEventListener('click', async function (e) {
     const target = e.target;
 
-    // --- 1. Bắt nút Add to Cart ---
     if (target.closest('form[action*="/cart/add"], button[name="plus"], button[name="minus"], button[name="add"]')) {
         console.log('[CART] Submit Add to Cart form');
+
         await new Promise(r => setTimeout(r, 500));
-        checkGift(); // Gọi hàm của bạn
+
+        checkGift();
     }
 });
