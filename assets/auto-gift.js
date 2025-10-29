03b709b085dff16fc03f800e9ba9c05521814aaf
Duc Anh <ducanh260204@gmail.com>
2025-10-29 03:47:42 +0000

    Update ajax

--- assets/auto-gift.js
+++ assets/auto-gift.js
@@ -38,6 +38,31 @@ async function checkGift() {
 }
 
 checkGift();
+// BẮT MỌI AJAX CART (ngắn gọn)
+(function () {
+    const RE = /\/cart\/(add|update|change|clear)(?:\.js)?(?:\?|$)/i;
+    const isMut = (url, method = 'GET') => RE.test(String(url)) && /POST|PUT|PATCH|DELETE/i.test(method);
+
+    let t; const bump = () => { clearTimeout(t); t = setTimeout(() => checkGift(), 80); };
+
+    // Hook fetch
+    const _fetch = window.fetch;
+    window.fetch = function (i, init) {
+        const url = i instanceof Request ? i.url : String(i || '');
+        const method = (i instanceof Request ? i.method : (init && init.method)) || 'GET';
+        const mutated = isMut(url, method);
+        return _fetch.apply(this, arguments).then(res => { if (mutated && res.ok) bump(); return res; });
+    };
+
+    // Hook XHR
+    const XO = XMLHttpRequest.prototype.open;
+    const XS = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.open = function (m, u) { this.__mut = isMut(u, m); return XO.apply(this, arguments); };
+    XMLHttpRequest.prototype.send = function () {
+        if (this.__mut) this.addEventListener('load', () => { if (this.status >= 200 && this.status < 300) bump(); });
+        return XS.apply(this, arguments);
+    };
+})();
 
 // Theo dõi khi người dùng thay đổi giỏ
 document.addEventListener('click', function (e) {
